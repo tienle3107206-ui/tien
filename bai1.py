53426b38b60bcdf096336d535fec9b1c2b70eaab
tien <tienle3107206@gmail.com>
2025-11-10 16:00:09 +0700

    first commit

--- bai1.py
+++ bai1.py
@@ -1,6 +1,10 @@
-try:
-    so = int(input('nhap so: '))
-    if so % 2 == 0
-    print(f"{so} la so chan")
-    else
-    print(f"{so} la so le")
+thang_31 = [1, 3, 5, 7, 8, 10, 12]
+
+m = int(input("Nhập số tháng (1-12): "))
+
+if m in thang_31:
+    print(f"Tháng {m} có 31 ngày.")
+elif m == 2:
+    print("Tháng 2 có 28 hoặc 29 ngày.")
+else:
+    print(f"Tháng {m} có 30 ngày.")
